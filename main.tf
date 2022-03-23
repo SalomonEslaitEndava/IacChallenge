e4267bc41b602f2c8a3dcc25a094305ebe8099ed
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-03-23 13:37:09 -0500

    re create all network interface

--- main.tf
+++ main.tf
@@ -23,38 +23,55 @@ provider "google-beta" {
   zone        = "us-west1-a"
 }
 
-module "test-vpc-module" {
-  source       = "terraform-google-modules/network/google"
-  version      = "~> 4.0.1"
-  project_id   = "iac-challenge"
-  network_name = "iac-vpc"
-  mtu          = 1460
-
-  subnets = [
-    {
-      subnet_name   = "subnet-iac"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = "us-west1"
-    }
-  ]
+resource "google_compute_network" "iac-network" {
+  name                    = "iac-network"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "vm-iac"
-  machine_type = "n1-standart-2"
-  zone         = "us-west1-a"
-  
+resource "google_compute_subnetwork" "iac-subnet" {
+  name                     = "iac-subnet"
+  ip_cidr_range            = "10.0.0.0/24"
+  network                  = google_compute_network.iac-network.self_link
+  region                   = "us-west1"
+  private_ip_google_access = true
+}
+
+resource "google_compute_instance" "default" {
+  count                     = 1
+  name                      = "iac-vm"
+  zone                      = "us-west1-a"
+  # tags                      = ["${concat(list("${var.name}-ssh", "${var.name}"), var.node_tags)}"]
+  machine_type              = "f1-micro"
+  # min_cpu_platform          = "${var.min_cpu_platform}"
+  allow_stopping_for_update = true
+
   boot_disk {
+    #auto_delete = "${var.disk_auto_delete}"
+
     initialize_params {
       image = "centos-cloud/centos-7"
+      #size  = "${var.disk_size_gb}"
+      #type  = "${var.disk_type}"
     }
   }
+
   network_interface {
-    network = module.test-vpc-module.network_name
-    subnetwork   = module.test-vpc-module.subnets[subnet_name]
-    access_config {
-    }
+    subnetwork    = google_compute_subnetwork.subnet.name
+    access_config = {}
+    # address       = "${var.network_ip}"
   }
+
 }
 
+resource "google_compute_firewall" "ssh" {
+  name    = "ssh-rule"
+  network = google_compute_subnetwork.subnet.name
 
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  #source_tags = ["${var.name}-bastion"]
+  #target_tags = ["${var.name}-ssh"]
+}
