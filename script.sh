914e9d9cfd849ac9575e53235ae91f58ef1f61ed
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-03-23 17:43:11 -0500

    adding first variables

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 sudo yum -y install httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd.service
-sudo echo "<html><body>Hola soy Salomon Eslait y esto es mi IaC </body></html>" >> /var/www/html/index.html 
+sudo echo "<html><body>Hola soy Salomon Antonio Eslait y esto es mi IaC </body></html>" >> /var/www/html/index.html 
 sudo systemctl restart httpd 
