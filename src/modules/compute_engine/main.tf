d880e7c7dc919483740c05591192d0bc6c399606
SalomonEslaitEndava <salomon-antonio.eslait@endava.com>
2022-03-28 15:30:29 -0500

    :zap: add outpouts

--- src/modules/compute_engine/main.tf
+++ src/modules/compute_engine/main.tf
@@ -1,26 +1,21 @@
 resource "google_compute_instance" "default" {
-  count = var.instance_count # 1
-  name  = var.instance_name  #"iac-vm"
-  zone  = var.instance_zone  # "us-west1-a"
+  count = var.instance_count
+  name  = var.instance_name
+  zone  = var.instance_zone
   # tags                      = ["${concat(list("${var.name}-ssh", "${var.name}"), var.node_tags)}"]
-  machine_type = var.machine_type #"f1-micro"
-  # min_cpu_platform          = "${var.min_cpu_platform}"
-  allow_stopping_for_update = var.allow_stopping_for_update # true
+  machine_type = var.machine_type
+  allow_stopping_for_update = var.allow_stopping_for_update
 
   boot_disk {
-    #auto_delete = "${var.disk_auto_delete}"
 
     initialize_params {
-      image = var.instance_image #"centos-cloud/centos-7"
-      #size  = "${var.disk_size_gb}"
-      #type  = "${var.disk_type}"
+      image = var.instance_image
     }
   }
 
   network_interface {
-    subnetwork = "iac-subnet" #google_compute_subnetwork.iac-subnet.name
+    subnetwork = module.networking.subnet_name #"iac-subnet" #google_compute_subnetwork.iac-subnet.name
     access_config {}
-    # address       = "${var.network_ip}"
   }
 
   metadata_startup_script = file("script.sh")
